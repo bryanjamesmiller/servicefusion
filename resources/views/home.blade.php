0affb61e94713c1d65d2dbed4cc560900408beed
Bryan Miller <gratiafide@yahoo.com>
2015-12-14 20:42:35 -0600

    Changed a hyperlink's text

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -22,7 +22,7 @@
         <li>Javascript data validation and warnings for user input on both the create and update pages (public/js/custom.js).</li>
         <li>Server-side data validation on both the create and update pages (<span class="allow_wrap">app/Http/Requests/UserRegistrationRequest.php</span>).  Try entering an invalid date to see the server-side input validation at work.</li>
         <li>Made use of the Google Maps API to produce some details about the zip codes that get entered (displayed on the <a href="http://servicefusion.grat.xyz/records">http://servicefusion.grat.xyz/records</a> page).</li>
-        <li>Responsive design using Bootstrap 3 framework - try resizing http://servicefusion.grat.xyz/records</li>
+        <li>Responsive design using Bootstrap 3 framework - try resizing <a href="http://servicefusion.grat.xyz/records">the "Complete list of records" page</a>, for example.</li>
         <li>Javascript injection protection through my custom-made middleware (<span class="allow_wrap">app/Http/Middleware/NoBracketsAllowed.php</span>).  Try entering a <code>&lt;script&gt;</code> tag into one of the input fields and try to submit it to see the result.</li>
         <li>Automatic SQL injection protection through Eloquent ORM (Object-relational mapping).</li>
         <li>Cross-site request forgery (CSRF) protection by using Laravel forms correctly.</li>
